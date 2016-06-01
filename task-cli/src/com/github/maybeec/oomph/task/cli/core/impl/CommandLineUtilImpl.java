e9dc11a6983f6678db652d41322d9ab8734a7fab
Steffen Holzer <steffenbholzer@googlemail.com>
2016-06-01 19:49:43 +0200

    #4 Integrated check for windows. Current implementation definitely needs some testing

--- task-cli/src/com/github/maybeec/oomph/task/cli/core/impl/CommandLineUtilImpl.java
+++ task-cli/src/com/github/maybeec/oomph/task/cli/core/impl/CommandLineUtilImpl.java
@@ -45,15 +45,26 @@ public class CommandLineUtilImpl implements CommandLineUtil
   public void execute(Iterable<String> cmdLine) throws CommandLineUtilException
   {
     SetupTaskLogger.getLogger().logInfo("Execute: " + stringListToLine(cmdLine));
-    try
+
+    List<String> commands = new LinkedList<String>();
+
+    if (OsUtil.isWindows())
     {
-      List<String> commands = new LinkedList<String>();
       commands.add("CMD");
       commands.add("/C");
-      for (String s : cmdLine)
-      {
-        commands.add(s);
-      }
+    }
+    else
+    {
+      // I haven't found a necessary prefix as it is needed for windows. Maybe this just magically works
+    }
+
+    for (String s : cmdLine)
+    {
+      commands.add(s);
+    }
+
+    try
+    {
       SetupTaskLogger.getLogger().log("Passing '" + stringListToLine(commands) + "' to ProcessBuilder");
       ProcessBuilder pb = new ProcessBuilder(commands);
       pb.redirectErrorStream(true);
